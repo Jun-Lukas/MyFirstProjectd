7b074229f81a34371215730680db6e889ad3f3b6
zhaojun <zj_lukas@hotmail.de>
2021-08-21 22:19:00 +0200

    add a added function.

--- hello.c
+++ hello.c
@@ -1,7 +1,13 @@
 #include <stdio.h>
 
-main(){
+int main(){
 	printf(" hecllo world" );
 printf("i like you");
+printf("i dont like you");
 	return 0;
+}
+
+int add(int a, int b){
+
+	return a + b;
 }
